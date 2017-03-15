0756cb44edb00a1f6afae908ae90b87dd935b1c5
MOZART <wfwdave@gmail.com>
2017-03-16 00:10:14 +0600

    init for the riscafe theme

---
+++ wp-content/themes/riscafe/footer.php
@@ -0,0 +1,18 @@
+<div class="footer">
+    <div class="site_wrapper">
+        <div class="footer-container">
+            <div class="plus-block">
+                <img src="images/plus1.png" alt="">
+                <span>Доставка до 12 ночи</span>
+            </div>
+            <div class="plus-block">
+                <img src="images/plus2.png" alt="">
+                <span>Доставка по Алматы меньше чем за час </span>
+            </div>
+            <div class="plus-block">
+                <img src="images/plus3.png" alt="">
+                <span>Гарантируем, вам понравятся наши блюда</span>
+            </div>
+        </div>
+    </div>
+</div>
