b9a95b1db153ec6b51a98f54b67dc6692ab898fe
MOZART <wfwdave@gmail.com>
2017-03-17 00:24:47 +0600

    главная и страница категорий

--- wp-content/themes/riscafe/footer.php
+++ wp-content/themes/riscafe/footer.php
@@ -2,15 +2,15 @@
     <div class="site_wrapper">
         <div class="footer-container">
             <div class="plus-block">
-                <img src="images/plus1.png" alt="">
+                <img src="<?=esc_url( get_template_directory_uri() ); ?>/images/plus1.png" alt="">
                 <span>Доставка до 12 ночи</span>
             </div>
             <div class="plus-block">
-                <img src="images/plus2.png" alt="">
+                <img src="<?=esc_url( get_template_directory_uri() ); ?>/images/plus2.png" alt="">
                 <span>Доставка по Алматы меньше чем за час </span>
             </div>
             <div class="plus-block">
-                <img src="images/plus3.png" alt="">
+                <img src="<?=esc_url( get_template_directory_uri() ); ?>/images/plus3.png" alt="">
                 <span>Гарантируем, вам понравятся наши блюда</span>
             </div>
         </div>
--- wp-content/themes/riscafe/functions.php
+++ wp-content/themes/riscafe/functions.php
@@ -1,1 +1,13 @@
 <?php
+
+if ( ! function_exists( 'riscafe_category_item' ) ) :
+    function riscafe_category_item($category) {
+        $result = [];
+        $result[] = '<div class="menu-item">';
+        $result[] = '<a href="'.get_term_link($category).'"><img src="'.z_taxonomy_image_url(get_term($category)->term_id).'" alt="">';
+        $result[] = '<a href="'.get_term_link($category).'" class="menu-item--link">'.$category->name.' ></a>';
+        $result[] = '</div>';
+
+        return join("\n", $result);
+    }
+endif;
