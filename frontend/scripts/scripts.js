9ed8159e88ce5b1305130a5dfa3d9e08d72faf14
Margarita <blinkova.margarita@gmail.com>
2017-03-19 00:19:02 +0600

    cart page

--- frontend/scripts/scripts.js
+++ frontend/scripts/scripts.js
@@ -1,22 +1,50 @@
 $(document).ready(function() {
-	var sidecartOpen = function(){
-		$('.cart-overlay').toggleClass('open');
-		$('.sideCart').toggleClass('open');
-	};
+    var sidecartOpen = function() {
+        $('.cart-overlay').toggleClass('open');
+        $('.sideCart').toggleClass('open');
+    };
 
 
-	$('.add-to-cart').on('click', function(e){
-		e.preventDefault();
-		sidecartOpen();
-	});
+    $('.add-to-cart').on('click', function(e) {
+        e.preventDefault();
+        sidecartOpen();
+    });
 
-	$('.cart-overlay').on('click', function(e){
-		e.preventDefault();
-		sidecartOpen();
-	});
+    $('.cart-overlay').on('click', function(e) {
+        e.preventDefault();
+        sidecartOpen();
+    });
 
-	$('.closeCart').on('click', function(e){
-		e.preventDefault();
-		sidecartOpen();
-	});
+    $('.closeCart').on('click', function(e) {
+        e.preventDefault();
+        sidecartOpen();
+    });
+
+
+    $('.quont-minus').click(function() {
+        var $input = $(this).parent().find('input');
+        var count = parseInt($input.val()) - 1;
+        count = count < 1 ? 1 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+    });
+    $('.quont-plus').click(function() {
+        var $input = $(this).parent().find('input');
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        return false;
+    });
+
+    $('.add-comment').on('click', function(e){
+    	e.preventDefault();
+    	$(this).hide();
+    	$('.comment-form').show();
+    });
+
+    $('.cancel-comment').on('click', function(e){
+    	e.preventDefault();
+    	$('.comment-form').hide();
+    	$('.add-comment').show();
+    });
 });
